c8c34b4833a88a98c9c65f951470f7cebbbfb6c9
giovagior <giovanni.giorgio@ingenieria.uner.edu.ar>
2024-08-07 12:11:27 -0300

    Inicio de resolucion del Ej3 de la Guia1

--- firmware/projects/Guia1_Ej3/main/Guia1_Ej3.c
+++ firmware/projects/Guia1_Ej3/main/Guia1_Ej3.c
@@ -29,6 +29,13 @@
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
+struct leds
+{
+      uint8_t mode;       ON, OFF, TOGGLE
+	uint8_t n_led;        indica el número de led a controlar
+	uint8_t n_ciclos;   indica la cantidad de ciclos de ncendido/apagado
+	uint16_t periodo;    indica el tiempo de cada ciclo
+} my_leds; 
 
 /*==================[internal functions declaration]=========================*/
 
