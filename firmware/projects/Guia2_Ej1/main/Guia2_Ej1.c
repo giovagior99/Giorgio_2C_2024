51ada51c8e33963ffbcbca488c4a033cf80997df
giovagior <giovanni.giorgio@ingenieria.uner.edu.ar>
2024-08-28 10:22:15 -0300

    Se crea el proyector correspondiente al Ej 1 de la Guia 2

---
+++ firmware/projects/Guia2_Ej1/main/Guia2_Ej1.c
@@ -0,0 +1,40 @@
+/*! @mainpage Guia2_Ej1
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |    ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	  ECHO	 	| 	 GPIO_3		|
+ * | 	 TRIGGER 	| 	 GPIO_2		|
+ * |      +5V	 	| 	  +5V		|
+ * | 	  GND	 	| 	  GND		|
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 28/08/2024 | Document creation		                         |
+ *
+ * @author Giovanni Giorgio (giovanni.giorgio@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	
+}
+/*==================[end of file]============================================*/
