c2cc44e8c5575de9bf4b28ea4ce293ee7f23dcb3
giovagior <giovanni.giorgio@ingenieria.uner.edu.ar>
2024-09-04 10:58:08 -0300

    Se agrega documentacion de Doxygen del Ej 1 de la Guia 2

---
+++ firmware/projects/Guia2_Ej1/doc/html/_guia2___ej1_8c.js
@@ -0,0 +1,13 @@
+var _guia2___ej1_8c =
+[
+    [ "CONFIG_MEASURE_PERIOD", "_guia2___ej1_8c.html#a9a265a2fa8b8a1442be25098dec84dac", null ],
+    [ "CONFIG_MODE_PERIOD", "_guia2___ej1_8c.html#a84f866c95c40f50663299f8bfae6c68c", null ],
+    [ "CONFIG_SHOW_PERIOD", "_guia2___ej1_8c.html#a635e28527e5512230ae4371fe42fbea7", null ],
+    [ "app_main", "_guia2___ej1_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "distancia", "_guia2___ej1_8c.html#aec40b15ff3098bdb2425eb4dc9c11a18", null ],
+    [ "medir_task_handle", "_guia2___ej1_8c.html#a2b0cb40d7a31ce7e8b3f906fd55df312", null ],
+    [ "modo_task_handle", "_guia2___ej1_8c.html#ac0b9486e9468f3c9ac74b74bf5d8f20a", null ],
+    [ "mostrar_task_handle", "_guia2___ej1_8c.html#a1316d5b1a3e4d9dee5085faf3b582e40", null ],
+    [ "tecHOLD", "_guia2___ej1_8c.html#a9496beb444b6ade6a9e263ec1f9352f5", null ],
+    [ "tecON", "_guia2___ej1_8c.html#ab87fdd4b4d8f8a7c4f9a99c8354b802d", null ]
+];
---
+++ firmware/projects/Guia2_Ej1/doc/html/navtreedata.js
@@ -0,0 +1,76 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Guia2_Ej1", "index.html", [
+    [ "General Description", "index.html#genDesc", null ],
+    [ "Hardware Connection", "index.html#hardConn", null ],
+    [ "Changelog", "index.html#changelog", null ],
+    [ "Template", "md__c_1_2_repositorios_2_giorgio__2_c__2024_2firmware_2projects_2_guia2___ej1_2_r_e_a_d_m_e.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Estructuras de datos", "annotated.html", [
+      [ "Estructuras de datos", "annotated.html", "annotated_dup" ],
+      [ "Índice de estructuras de datos", "classes.html", null ],
+      [ "Campos de datos", "functions.html", [
+        [ "Todos", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Archivos", "files.html", [
+      [ "Lista de archivos", "files.html", "files_dup" ],
+      [ "Globales", "globals.html", [
+        [ "Todos", "globals.html", "globals_dup" ],
+        [ "Funciones", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "typedefs", "globals_type.html", null ],
+        [ "Enumeraciones", "globals_enum.html", null ],
+        [ "Valores de enumeraciones", "globals_eval.html", "globals_eval" ],
+        [ "defines", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_guia2___ej1_8c.html",
+"bootloader_2config_2sdkconfig_8h.html#a2d38a5dbf32641f9b64b4afc4abf6ecf",
+"bootloader_2config_2sdkconfig_8h.html#a92d6b4671dc4d1f2ca7806ec47064904",
+"bootloader_2config_2sdkconfig_8h.html#afeadf9134128696d89bb3695de972b33",
+"config_2sdkconfig_8h.html#a405c740d8fa70a3befd2eede7346081a",
+"config_2sdkconfig_8h.html#a85d22568892b2a6f49a209ef72786c03",
+"config_2sdkconfig_8h.html#ac744772ef23911ed780cdcf7fb8baa88",
+"dir_989e28f8680015ea565bb406b86086dd.html",
+"group___b_l_e.html#ggaed078c594a86d36a49f51710e35c405ba02f14e6303b4d2e4e46747324d649dda",
+"group___g_i_o_p.html#ga3f43faf280a63697678036bdbd434c46",
+"group___m_p_u6050.html#ga0f9622a82b552e2272079690d603829b",
+"group___m_p_u6050.html#ga7ef3e72f5de0daaec883a4f1440350ce",
+"group___m_p_u6050.html#gaf757b337125ce76f74d451fdb5811cc6",
+"ili9341_8c.html#ae9a82b50c8748942c64e90f7f8629669",
+"structhid__dev__cfg__t.html#a11563208acc8e846560342314084748d"
+];
+
+var SYNCONMSG = 'pulsar para deshabilitar sincronización';
+var SYNCOFFMSG = 'pulsar para habilitar sincronización';
