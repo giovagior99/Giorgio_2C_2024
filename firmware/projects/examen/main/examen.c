15f5e9f468254a6f8e243567852c5483ac9ac7b4
giovagior <giovanni.giorgio@ingenieria.uner.edu.ar>
2024-11-04 16:13:55 -0300

    Se inicializan el HC-SR04, los leds y los timers; se configuran los paremetros para los timers

--- firmware/projects/examen/main/examen.c
+++ firmware/projects/examen/main/examen.c
@@ -36,13 +36,68 @@
 #include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
 
+/** @def CONFIG_MEDIR_PERIOD
+ * @brief Periodo de tiempo de refresco de medición del HC-SR04 (500 ms)
+ */
+#define CONFIG_MEDIR_PERIOD 500000 //en microsegundos
+
+/** @def CONFIG_NOTIFICAR_PERIOD
+ * @brief Periodo de tiempo de refresco de notificacion en leds y buzzer (500 ms)
+ */
+#define CONFIG_NOTIFICAR_PERIOD 500000 //en microsegundos
+
 /*==================[internal data definition]===============================*/
+
+/** @def medir_task_handle
+ *  @brief
+ */
+TaskHandle_t medir_task_handle = NULL;
+
+/** @def mostrar_task_handle
+ * @brief
+ */
+TaskHandle_t notificar_task_handle = NULL;
+
+/** @def comunicar_task_handle
+ * @brief
+ */
+TaskHandle_t comunicar_task_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
 
+void FuncTimerA(void* param){
+    
+}
+
+/**  @def void FuncTimerB(void* param)
+ * @brief Función invocada en la interrupción del timer B
+ */
+void FuncTimerB(void* param){
+   
+}
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
+
+	/* initializations */
+	HcSr04Init(GPIO_3, GPIO_2);
+	LedsInit();
+
+	timer_config_t timer_medicion = {
+        .timer = TIMER_A,
+        .period = CONFIG_MEDIR_PERIOD,
+        .func_p = FuncTimerA,
+        .param_p = NULL
+    };
+    TimerInit(&timer_medicion);
+
+	timer_config_t timer_notificacion = {
+        .timer = TIMER_B,
+        .period = CONFIG_NOTIFICAR_PERIOD,
+        .func_p = FuncTimerB,
+        .param_p = NULL
+    };
+    TimerInit(&timer_notificacion);
 
 }
 /*==================[end of file]============================================*/
