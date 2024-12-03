b48ba33b42f7768adb3f686d7f97a88faf72c453
giovagior <giovanni.giorgio@ingenieria.uner.edu.ar>
2024-12-03 10:11:08 -0300

    Se crea el proyecto examen_final

---
+++ firmware/projects/examen_final/main/examen_final.c
@@ -0,0 +1,37 @@
+/*! @mainpage examen_final
+ *
+ * @section genDesc General Description
+ *
+ * Examen final de la asignatura Electronica Programable 03/12/2024
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 03/12/2024 | Document creation		                         |
+ *
+ * @author Giovanni Giorgio (giovanni.giorgio@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	
+}
+/*==================[end of file]============================================*/
